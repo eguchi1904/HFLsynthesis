3db7afe7653c55f94a8e2e97cb1232e11541f479
Eguchi <eguchi.1904@gmail.com>
2019-10-31 21:26:37 +0900

    to_hfl_clause

--- parser/mkEpsiron.ml
+++ parser/mkEpsiron.ml
@@ -1,8 +1,58 @@
 open ParseSyntax
 
+let separete_params_from_application_args
+    :predicateArg list -> Hfl.clause list -> Hfl.abstClause list * Hfl.clause list = 
+  (fun predicate_args cs ->
+  (assert (List.length predicate_args  = List.length cs));
+  List.fold_right2
+    (fun predicate_arg clause (acc_param, acc_arg) ->
+      if predicate_arg.is_param then
+        match clause with
+        | `Abs _ as abs_clause -> (abs_clause::acc_param, acc_arg)
+        | _ -> assert false
+      else
+        (acc_param, clause::acc_arg)
+    )
+    predicate_args
+    cs
+    ([], [])
+  )
 
-let to_hfl_clause:predicateDef M.t -> clause -> Hfl.clause =
-  (fun pmap c -> assert false)
+
+let rec to_hfl_clause_abs pmap  (`Abs (args, c)) =  `Abs (args, to_hfl_clause pmap c)
+  
+and to_hfl_clause:predicateDef M.t -> clause -> Hfl.clause =
+  (fun pmap c ->
+    match c with
+    | `Abs _ as abs_c -> (to_hfl_clause_abs pmap abs_c:>Hfl.clause)
+    | `Base base -> `Base base
+    | `App {head = head; args = cs} ->
+       let hfl_cs = List.map (to_hfl_clause pmap) cs in
+       (match M.find_opt head pmap with
+        |Some head_def ->
+          let params, args =
+            separete_params_from_application_args
+              head_def.args
+              hfl_cs
+          in
+          `App {head = head; params = params; args = args}
+        |None -> assert false)
+  
+    | `RData (id, abst_cs, c) ->
+       `RData (id,
+               List.map (to_hfl_clause_abs pmap) abst_cs,
+               to_hfl_clause pmap c)
+
+    | `Or (c1, c2) ->
+       `Or (to_hfl_clause pmap c1,
+            to_hfl_clause pmap c2)
+
+    | `And (c1, c2) ->
+       `And (to_hfl_clause pmap c1,
+            to_hfl_clause pmap c2)
+  )
+          
+          
 
 let separate_params: predicateArg list -> (Id.t * Hfl.abstSort) list * (Id.t * Hfl.sort) list =
   (fun predicate_args -> assert false)
